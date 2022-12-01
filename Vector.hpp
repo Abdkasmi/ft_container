d52718f9306371d17b9b5091b6c583bfabe89a67
Abdoullah Kasmi <abdoullah@MacBook-Pro-de-Abdoullah.local>
2022-12-01 14:22:09 +0100

    need to recheck the reallocation when size > capacity

--- Vector.hpp
+++ Vector.hpp
@@ -59,7 +59,7 @@ namespace ft {
 				}
 			vector(const vector& x): _allocator(x._allocator), _size(x._size), _lenght(x._lenght) {
 				this->_begin = this->_allocator.allocate(this->_lenght, 0);
-				for (size_type i = 0; i < this->_size, i++)
+				for (size_type i = 0; i < this->_size; i++)
 					this->_allocator.construct(this->_begin + i, *(x._begin + i));
 			}
 
@@ -96,21 +96,162 @@ namespace ft {
 			}
 
 			reverse_iterator rbegin() {
-
+				return (reverse_iterator(this->_begin + this->_size));
 			}
 
 			reverse_iterator rend() {
-
+				return (reverse_iterator(this->_begin));
 			}
 
-			const_reverse_iterator rbegin() {
-
+			const_reverse_iterator rbegin() const {
+				return (const_reverse_iterator(this->_begin + this->_size));
 			}
 
-			const_reverse_iterator rend() {
-				
+			const_reverse_iterator rend() const {
+				return (reverse_iterator(this->_begin));
 			}
 
+			/*
+			 ######     ###    ########     ###     ######  #### ######## ##    ##
+			##    ##   ## ##   ##     ##   ## ##   ##    ##  ##     ##     ##  ##
+			##        ##   ##  ##     ##  ##   ##  ##        ##     ##      ####
+			##       ##     ## ########  ##     ## ##        ##     ##       ##
+			##       ######### ##        ######### ##        ##     ##       ##
+			##    ## ##     ## ##        ##     ## ##    ##  ##     ##       ##
+			 ######  ##     ## ##        ##     ##  ######  ####    ##       ##
+			*/
+
+			size_type size() const {
+				return this->_size;
+			}
+
+			size_type max_size() const {
+				return this->_lenght;
+			}
+
+			void resize(size_type n, value_type val = value_type()) {
+				if (n < this->_size) {
+					for (size_type i = this->_size; i > n; i--)
+						this->_allocator.destroy(this->_begin + i);
+					this->_size = n;
+				}
+				else if (n > this->_size) {
+					if (n + this->_size <= this->_lenght) {
+						for (size_type i = this->_size; i < this->_size + n; i++) {
+							this->_allocator.allocator.construct(this->_begin + i, val);
+						}
+					}
+					else if (n + this->_size > this->_lenght) {
+						size_type tmp = this->_lenght;
+						this->_lenght *= 2;
+						this->_begin += this->_allocator.allocate(this->_lenght - tmp, 0);
+						for (size_type i = this->_size; i < this->_size + n; i++) {
+							this->_allocator.allocator.construct(this->_begin + i, val);
+						}
+					}
+				}
+			}
+
+			size_type capacity() const {
+				return this->_lenght;
+			}
+
+			bool empty() {
+				return !this->_size;
+			}
+
+			void reserve(type_type n) {
+				if (n > this->_lenght) {
+					pointer tmp = this->_allocator.allocate(n, 0);
+					for (size_type i = 0; i < n; i++)
+						this->_allocator.construct(tmp + i, this->_begin + i);
+					for (size_type i = 0; i < this->_size; i++)
+						this->_allocator.detsroy(this->_begin + i);
+					this->_allocator.dealocate(this->_begin, this->_lenght);
+					this->_begin = tmp;
+				}
+				else
+					return ;
+			}
+
+			/*
+			######## ##       ######## ##     ## ######## ##    ## ########       ###     ######   ######  ########  ######   ######
+			##       ##       ##       ###   ### ##       ###   ##    ##         ## ##   ##    ## ##    ## ##       ##    ## ##    ##
+			##       ##       ##       #### #### ##       ####  ##    ##        ##   ##  ##       ##       ##       ##       ##
+			######   ##       ######   ## ### ## ######   ## ## ##    ##       ##     ## ##       ##       ######    ######   ######
+			##       ##       ##       ##     ## ##       ##  ####    ##       ######### ##       ##       ##             ##       ##
+			##       ##       ##       ##     ## ##       ##   ###    ##       ##     ## ##    ## ##    ## ##       ##    ## ##    ##
+			######## ######## ######## ##     ## ######## ##    ##    ##       ##     ##  ######   ######  ########  ######   ######
+			*/
+
+			reference operator[](size_type n) {
+				return *this->_begin + n;
+			}
+
+			const_reference operator[](size_type n) const {
+				return *this->_begin + n;
+			}
+
+			reference at(size_type n) {
+				return *this->begin + n;
+			}
+
+			const_reference at(size_type n) const {
+				return *this->_begin + n;
+			}
+
+			reference front() {
+				return *this->_begin;
+			}
+
+			const_reference front() const {
+				return *this->_begin;
+			}
+
+			reference back() {
+				return *this->_begin + (this->_size - 1);
+			}
+
+			const_reference back() {
+				return *this->_begin + (this->_size - 1);
+			}
+
+			/*
+			##     ##  #######  ########  #### ######## #### ######## ########   ######
+			###   ### ##     ## ##     ##  ##  ##        ##  ##       ##     ## ##    ##
+			#### #### ##     ## ##     ##  ##  ##        ##  ##       ##     ## ##
+			## ### ## ##     ## ##     ##  ##  ######    ##  ######   ########   ######
+			##     ## ##     ## ##     ##  ##  ##        ##  ##       ##   ##         ##
+			##     ## ##     ## ##     ##  ##  ##        ##  ##       ##    ##  ##    ##
+			##     ##  #######  ########  #### ##       #### ######## ##     ##  ######
+			*/
+
+			template <class InputIterator>
+				void assign(InputIterator first, InputIterator last) {
+					if (this->_begin)
+						this->_allocator.dealocate(this->_begin, this->_lenght);
+					this->_size = last - first;
+					this->_lenght = this->_size * 2;
+					this->_begin = this->_allocator.allocate(this->_lenght, 0);
+					for (size_type i = 0; i < this->_size; i++)
+						this->_allocator.construct(this->_begin + i, *first++);
+				}
+
+			void assign(size_type n, const value_type & val) {
+				if (this->_begin)
+					this->_allocator.dealocate(this->_begin, this->_lenght);
+				this->_size = n;
+				this->_lenght = n * 2;
+				this->_begin = this->_allocator.allocate(this->_lenght, 0);
+				for (size_type i = 0; i < n; i++)
+					this->_allocator.constrcut(this->_begin + i, val[i]);
+			}
+
+			void push_back(const value_type & val) {
+				if (this->_size + 1 > this->_lenght)
+					this->_begin += 
+			}
+			// REFAIRE TOUT LES REALLOCS
 
 			private:
 
