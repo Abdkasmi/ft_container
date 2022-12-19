662cc4a55c62c03554f21fdbb71e230bfa451f9e
Abdoullah Kasmi <abdoullah@MacBook-Pro-de-Abdoullah.local>
2022-12-19 10:30:34 +0100

    map container

--- Map.hpp
+++ Map.hpp
@@ -23,12 +23,22 @@ namespace ft {
 		##     ## ######## ##     ## ########  ######## ##     ##       ##       ##    ##        ########
 		*/
 
-		typedef Key										key_type;
-		typedef T										mapped_type;
-		typedef	ft::pair<const key_type,mapped_type>	value_type;
-		typedef Compare									key_compare;
-		typedef 
-
+		typedef Key												key_type;
+		typedef T												mapped_type;
+		typedef ft::pair<const key_type,mapped_type>			value_type;
+		typedef Compare											key_compare;
+		// need to do value_comp not understood how to do it
+		typedef Alloc											allocator_type;
+		typedef typename allocator_type::reference				reference;
+		typedef typename allocator_type::const_reference		const_reference;
+		typedef typename allocator_type::pointer				pointer;
+		typedef typename allocator_type::const_pointer			const_pointer;
+		typedef typename ft::iterator::value_type				iterator;
+		typedef typename ft::const_iterator::value_type			const_iterator;
+		typedef typename ft::reverse_iterator<iterator>			reverse_iterator;
+		typedef typename ft::reverse_iterator<const_iterator>	const_reverse_iterator;
+		typedef ptrdiff_t										difference_type;
+		typedef size_t											size_type;
 
 		/*
 		##     ## ######## ##     ## ########  ######## ########     ######## ##     ## ##    ##  ######  ######## ####  #######  ##    ##  ######
--- Utils.hpp
+++ Utils.hpp
@@ -54,4 +54,62 @@ namespace ft {
         return (first2!=last2);
         }
 
+    /*
+    ##     ##    ###    ##       ##     ## ########          ######   #######  ##     ## ########
+    ##     ##   ## ##   ##       ##     ## ##               ##    ## ##     ## ###   ### ##     ##
+    ##     ##  ##   ##  ##       ##     ## ##               ##       ##     ## #### #### ##     ##
+    ##     ## ##     ## ##       ##     ## ######           ##       ##     ## ## ### ## ########
+     ##   ##  ######### ##       ##     ## ##               ##       ##     ## ##     ## ##
+      ## ##   ##     ## ##       ##     ## ##               ##    ## ##     ## ##     ## ##
+       ###    ##     ## ########  #######  ######## #######  ######   #######  ##     ## ##
+    */
+
+    template <class Key, class T, class Compare, class Alloc>
+    class map : public binary_function<value_type,value_type,bool> {   // in C++98, it is required to inherit binary_function<value_type,value_type,bool>
+        friend class map;
+        protected:
+        Compare comp;
+        value_compare (Compare c) : comp(c) {}
+        public:
+        typedef bool result_type;
+        typedef value_type first_argument_type;
+        typedef value_type second_argument_type;
+        bool operator() (const value_type& x, const value_type& y) const
+        {
+            return comp(x.first, y.first);
+        }
+    };
+
+    /*
+    ######## ##    ##    ###    ########  ##       ########    #### ########
+    ##       ###   ##   ## ##   ##     ## ##       ##           ##  ##
+    ##       ####  ##  ##   ##  ##     ## ##       ##           ##  ##
+    ######   ## ## ## ##     ## ########  ##       ######       ##  ######
+    ##       ##  #### ######### ##     ## ##       ##           ##  ##
+    ##       ##   ### ##     ## ##     ## ##       ##           ##  ##
+    ######## ##    ## ##     ## ########  ######## ########    #### ##
+    */
+
+    template <bool Cond, class T = void>
+        struct enable_if {};
+    template<class T> 
+        struct enable_if<true, T> { typedef T type; };
+
+    /*
+    ####  ######     #### ##    ## ######## ########  ######   ########     ###    ##
+     ##  ##    ##     ##  ###   ##    ##    ##       ##    ##  ##     ##   ## ##   ##
+     ##  ##           ##  ####  ##    ##    ##       ##        ##     ##  ##   ##  ##
+     ##   ######      ##  ## ## ##    ##    ######   ##   #### ########  ##     ## ##
+     ##        ##     ##  ##  ####    ##    ##       ##    ##  ##   ##   ######### ##
+     ##  ##    ##     ##  ##   ###    ##    ##       ##    ##  ##    ##  ##     ## ##
+    ####  ######     #### ##    ##    ##    ########  ######   ##     ## ##     ## ########
+    */
+
+    template <class T, T v>
+    struct integral_constant {
+        static const T value = v;
+        typedef T value_type;
+        typedef integral_constant<T,v> type;
+    };
+    
 }
